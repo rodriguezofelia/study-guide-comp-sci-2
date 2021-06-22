461a879c76dd37a8785f3a4cd3e2501a7699e9c7
Ofelia <oer.rodriguez@gmail.com>
2021-06-22 01:44:06 +0000

    Completed first section of discussion questions.

---
+++ practice/discussion.py
@@ -0,0 +1,16 @@
+# Recursion
+
+# 1) In your own words, what is recursion?
+# Recursion refers to a function that calls itself.
+
+# Why is it necessary to have a base case?
+# If there is no base case or it can't be reached, the recursion could go on forever 
+
+# Graphs
+# What is a graph?
+# It's a data structure that has nodes and edges. The nodes (or vertices) are connected by edges (or arcs). 
+# Nodes are units of data and the edges (arcs (think of link)) are the connections between the relationships of nodes.
+
+# Give an example of something that would be good to model with a graph.
+
+# A food chain that shows every single arrow indicating which animal preys on each other. 
