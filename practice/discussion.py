4aae9e36be48cb6b241bb358c14be8b25b1181eb
Ofelia <oer.rodriguez@gmail.com>
2021-06-25 03:37:10 +0000

    Completed discussion questions.

--- practice/discussion.py
+++ practice/discussion.py
@@ -14,3 +14,18 @@
 # Give an example of something that would be good to model with a graph.
 
 # A food chain that shows every single arrow indicating which animal preys on each other. 
+
+#Sorting
+# 1) Describe in words how the Bubble Sort algorithm works.
+# Considered one of the the simpliest algorithms to sort an array of objects. It works by comparing a
+# adjacent elements and swapping them if they are not already in lowest to highest order. 
+
+# 2) Describe in words how the Merge Sort algorithm works.
+# Merge sort is based on the divide and conquer technique, where a problem is subdivided into smaller
+# problems. Lets say we have an array of 4123, it will be divided into two arrays consisting of 41, 23.
+# Then we divide them again so it is 4, 1, 2, 3. Then they are added back into halves and sorted 14, 23. 
+# Lastly, the two halves are added together and sorted into 1234. 
+
+# 3) Describe in words how the Quick Sort algorithm works.
+# There is a pivot number that is used to move all smaller numbers (than pivot) to the beginning 
+# and all greater numbers to the right of the pivot number. 
